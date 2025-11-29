4234b6c9d94b2217ba28a565305bbe0f80228851
adarsh <adarshsingh4908@gmail.com>
2025-11-29 17:04:10 +0530

    first commit

---
+++ java script.js/java script.js
@@ -0,0 +1,97 @@
+// var a =10;
+// let a =20;
+// var dulha ="lab";
+// let dulhan ="laby";
+// const discount =20;//inetialozation and declaration//window mein add hoti han
+// console.log(dulha,dulhan)
+
+// //inetialppzation and declaration
+// //window mein add hoti hai
+// //scope(dlobal,block,functional)
+
+// var a = 12;//global scope
+// {
+//     var b =22;
+// }
+// console.log(b)//var is global
+
+// console.log(a)
+// {
+//     let c =12;//block scope
+//     console.log(c)
+// }
+// function abcd(){
+//     if(true){
+//         var c =12;//functional
+//     }
+//     else{
+
+//     }
+// }
+// reassignment//readeclaration 
+// var a = 12;
+// var a = 30;
+// a = 15;
+// console.log(a)
+// let d =12;
+// d = 20;
+// console.log(d)
+
+// //tdz(temporal dead zone)=>urtna area jitna mien js ko pata hai ki variable exist krta hai per vo aapko value mhi de sakta usse hi temporal zone kahte hain.
+// console.log(a);
+// console.log(b)
+// var a = 10;
+// let b =10;
+
+// hoisting=> ek variable ko kab  js  mien bnate hai to wo do hisso men toot jata hai and usko ddelare part upar vhla jata hai 
+// let value = "bye";
+// switch(value){
+//     case "hello":
+//     console.log("hi");
+//     break;
+  
+//     case "bye":
+//     console.log("two");
+//     break
+
+//     default:
+//         console.log("ivailid");
+// }
+// for( let i = 1; i<=10; i++){
+//     console.log("adarsh"+i);
+// }
+// let i =1;
+// while(i<32){
+//     console.log(i);
+// }
+// for(let i = 1; i<=10; i++){
+//     console.log(i)
+// }
+// let i =10;
+// while(i>=1){
+//     console.log(i);
+//     i--;
+// }
+// for(let i =30 ; i<=40; i++){
+//     if(i%2===0){
+//         console.log(i)
+//     }
+// }
+// let i = 1;
+// while(i<=15){
+//     console.log(i);
+//     i+=2;
+// }
+// let i = 1;
+
+// while(i <=15){
+
+//      if(i%2===1){
+//         console.log(i)
+//      }
+
+//      i++;
+// }
+for(let i = 5; i<=50; i+5){
+    console.log(i)
+   }
